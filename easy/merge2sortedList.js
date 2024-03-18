5c491ebf3d3052943f226a91e06428dbd3b3da7a
vuphuong1794 <phuongvu1794@gmail.com>
2024-03-18 19:54:45 +0700

    commit

---
+++ easy/merge2sortedList.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+            ListNode* list3, *temp;
+            list3 = new ListNode();
+            temp = list3;
+    
+            //khi cả 2 điều chưa duyệt hết
+            while(list1 && list2){
+                if(list1->val > list2->val){
+                    temp->next = list2;
+                    list2 = list2->next;
+                }
+            
+                else{
+                    temp->next=list1;
+                    list1 = list1->next;
+                }
+                temp=temp->next;
+            }
+    
+            if(list1) temp->next=list1;
+            if(list2) temp->next=list2;
+    
+            return list3->next;
+        }
+    };
